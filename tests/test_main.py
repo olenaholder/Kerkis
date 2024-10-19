b7a45cbff87b7ada9b9c4e8449867772e905db71
olenaholder <olenaholder1@gmail.com>
2024-10-19 04:14:59 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,10 @@
+# tests/test_main.py
+import unittest
+from src.main import main
 
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        self.assertIsNone(main())
+
+if __name__ == '__main__':
+    unittest.main()
